f0411f9ef5d4bf96a06478ceb1e81983e7201c37
Arek Jenerowicz <szejos@gmail.com>
2018-11-14 11:13:15 +0100

    Anagram edit

--- Anagram.cpp
+++ Anagram.cpp
@@ -22,18 +22,15 @@ bool check_if_anagram(const std::string &word_1, const std::string &word_2)
     for (int i = 0; i < word_1.length(); ++i)
     {
             occWord1[word_1[i]]++;
-
     }
     for (int i = 0; i < word_2.length(); ++i)
     {
             occWord2[word_2[i]]++;
-
     }
     for (int i = 0; i < 127; ++i)
     {
             if( occWord1[i] != occWord2[i] && i != 32) //spacja nie gra
                 return false;
-
     }
     return true;
 }
