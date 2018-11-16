7f5032a58380189447e8f0001caaae7ca7f30d9a
Arek Jenerowicz <szejos@gmail.com>
2018-11-16 12:27:24 +0100

    [Code Review] fix sort

--- FormTheLargest.c
+++ FormTheLargest.c
@@ -1,56 +1,53 @@
 #include <assert.h>
 #include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
 /**
 * Given a number, return the maximum number that could be formed with digits of the number given.
 * For example: number = 7389, return 9873
 */
+int compare (const void * a, const void * b) {
+     return ( *(int*)b - *(int*)a );
+}
 int form_the_largest_number(int number)
 {
-       int length = 0;
-       int tempN = number;
-       int returnNum = 0;
+       short length = 0;
+       int tempNumber = number;
+       int returnNum = 0, indeks = 0;
 
-       // Zamieniamy int na tablice
-       while (tempN != 0)
+       // getting size for malloc
+       while (tempNumber != 0)
        {
-          tempN /= 10;
+          tempNumber /= 10;
           length++;
        }
-       if(length != 0)
-       {
-          int *numberArray = (int*)malloc(sizeof(int) * length);
-          tempN = number;
-          int indeks = 0;
-          while (tempN != 0){
-              numberArray[indeks] = tempN % 10;
-              tempN /= 10;
-              indeks++;
-          }
 
-          // Sortowanie tablicy
-          for (int i = 0; i < length; i++)
-          {
-              for (int j = 0; j < length; j++)
-              {
-                  if (numberArray[j] < numberArray[i])
-                  {
-                      int tmp = numberArray[i];
-                      numberArray[i] = numberArray[j];
-                      numberArray[j] = tmp;
-                  }
-              }
-          }
+       if( length < 0 ) return 0;
 
-          // zamiana tablicy na inta
-          for(int i = 0; i < length; i++)
-          {
-              returnNum *=10;
-              returnNum+=numberArray[i];
-          }
-          free(numberArray);
+       int *numberArray = (int*)malloc(sizeof(int) * length);
+       tempNumber = number;
 
+
+       // convert int to array
+       while (tempNumber != 0){
+           numberArray[indeks] = tempNumber % 10;
+           tempNumber /= 10;
+           indeks++;
        }
 
+
+       // sorting table
+       qsort(numberArray, length, sizeof(int), compare);
+
+
+       // build integer
+       for(int i = 0; i < length; i++)
+       {
+           returnNum *= 10;
+           returnNum += numberArray[i];
+       }
+
+       free(numberArray);
 
     return returnNum;
 }
@@ -72,9 +69,8 @@ void test_cases()
 
 int main()
 {
-    test_cases();
 
-    //form_the_largest_number(341264);
+    test_cases();
 
 }
 
