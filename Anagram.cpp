1cfd269521c32335966636a8ce4c0d5094df1688
Arek Jenerowicz <szejos@gmail.com>
2018-11-16 15:26:24 +0100

    [Code Review] Anagram Fix

--- Anagram.cpp
+++ Anagram.cpp
@@ -1,35 +1,40 @@
 #include <assert.h>
 #include <string>
 #include <iostream>
+#include <algorithm>
 
 /**
 * Function returns true if word_1 and word_2 are anagrams to each other. Otherwise false.
 * Case sensitivity doesn't matter.
 */
+void countChars(int* table, const std::string &word)
+{
+    for (int i = 0; i < word.length(); ++i)
+    {
+            table[word.at(i) - '`']++;
+    }
+}
+
 bool check_if_anagram(const std::string &word_1, const std::string &word_2)
 {
+    const size_t sizeOfAsc = 26;
+    std::string tempWord_1 = word_1;
+    std::string tempWord_2 = word_2;
 
-    //if(word_1.length() != word_2.length() )
-     //   return false;
+    int occWord1[sizeOfAsc], occWord2[sizeOfAsc];
 
-    int occWord1[127], occWord2[127];
-    for (int i = 0; i < 127; ++i)
+    std::fill(occWord1, occWord1+sizeOfAsc, 0);
+    std::fill(occWord2, occWord2+sizeOfAsc, 0);
+
+    std::transform(tempWord_1.begin(), tempWord_1.end(), tempWord_1.begin(), ::tolower);
+    std::transform(tempWord_2.begin(), tempWord_2.end(), tempWord_2.begin(), ::tolower);
+
+    countChars(occWord1, tempWord_1);
+    countChars(occWord2, tempWord_2);
+
+    for (int i = 0; i < sizeOfAsc; ++i)
     {
-        occWord1[i] = 0;
-        occWord2[i] = 0;
-    }
-  
-    for (int i = 0; i < word_1.length(); ++i)
-    {
-            occWord1[word_1[i]]++;
-    }
-    for (int i = 0; i < word_2.length(); ++i)
-    {
-            occWord2[word_2[i]]++;
-    }
-    for (int i = 0; i < 127; ++i)
-    {
-            if( occWord1[i] != occWord2[i] && i != 32) //spacja nie gra
+            if( occWord1[i] != occWord2[i] && i != 32) //space
                 return false;
     }
     return true;
@@ -55,6 +60,8 @@ void test_cases()
     answer = check_if_anagram("F", "");
     assert(answer == false);
 
+    answer = check_if_anagram("Buckethead", "DeathCubeK");
+    assert(answer == true);
 }
 
 int main()
